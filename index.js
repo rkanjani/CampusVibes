85fdf18e3a80469eb21c8319f7ba28419c8403dd
nikhil <nperi104@uottawa.ca>
2016-07-23 14:30:08 -0400

    modified:   index.js 	deleted:    public/html/index.html 	deleted:    public/img/menu-shadow.png 	new file:   public/lang-logo.png 	deleted:    public/lib/aframe.js 	deleted:    public/lib/aframe.min.js 	deleted:    public/lib/jquery-3.1.0.min.js 	new file:   public/node.svg 	renamed:    public/css/main.css -> public/stylesheets/main.css

--- index.js
+++ index.js
@@ -5,10 +5,16 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
 app.get('/', function(request, response) {
-  response.render('../public/html/index.html');
+  response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+
